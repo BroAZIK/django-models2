8b725a194f8a6ac20d702c8202626cb86d476577
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-19 17:59:17 +0500

    product_table

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Product(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.id}. {self.name}"
     
--- api/views.py
+++ api/views.py
@@ -1,8 +1,20 @@
 from django.views import View
 from django.http import HttpRequest, JsonResponse
+from .models import Product
 
 
 
 class ProductView(View):
     def get(self, request: HttpRequest) -> JsonResponse:
-        pass
+        products = Product.objects.all()
+
+        results = []
+        for product in products:
+            results.append({
+                "id": product.id,
+                "name": product.name,
+                "description": product.description,
+                "price": product.price,
+            })
+
+        return JsonResponse(results, safe=False)

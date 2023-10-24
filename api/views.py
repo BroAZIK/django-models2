3666d28d9430d6eed1552904de185ba822f7002d
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-24 10:08:07 +0500

    get product view

--- api/views.py
+++ api/views.py
@@ -5,16 +5,38 @@ from .models import Product
 
 
 class ProductView(View):
-    def get(self, request: HttpRequest) -> JsonResponse:
-        products = Product.objects.all()
+    def get(self, request: HttpRequest, pk=None) -> JsonResponse:
+        if pk is None:
+            products = Product.objects.all()
 
-        results = []
-        for product in products:
-            results.append({
+            results = []
+            for product in products:
+                results.append({
+                    "id": product.id,
+                    "name": product.name,
+                    "description": product.description,
+                    "price": product.price,
+                })
+
+            return JsonResponse(results, safe=False)
+        else:
+            product = Product.objects.get(id=pk)
+            
+            results = {
                 "id": product.id,
                 "name": product.name,
                 "description": product.description,
                 "price": product.price,
-            })
+            }
+            return JsonResponse(results)
 
-        return JsonResponse(results, safe=False)
+    def post(self, request: HttpRequest) -> JsonResponse:
+        pass
+
+    def put(self, request: HttpRequest) -> JsonResponse:
+        pass
+
+    def delete(self, request: HttpRequest) -> JsonResponse:
+        pass
+
+    
